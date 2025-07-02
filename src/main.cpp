3c6130ef6c22abdc002ae95553c15bcdec325da1
West Port Automation <larrypc@shaw.ca>
2025-07-02 08:02:25 -0700

    add serial debug function

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <ioPins.h>
 
 MyDelay Scan(5000); // Create a MyDelay object with a 5-second delay
+MyDelay Debug(2000); // Create a MyDelay object with a 2-second delay
 
 // Declaire AlarmState states
 enum class AlarmState : uint8_t
@@ -11,18 +12,19 @@ enum class AlarmState : uint8_t
     Alarm,
     Bypass
 };
-AlarmState currentAlarmState = AlarmState::Normal; // Initialize the current alarm state to Normal
+AlarmState currentAlarmState = AlarmState::Normal; // Initialize an AlarmState variable to Normal
 
 // Function definitions
-void IsNormal(bool);
-void IsAlarm(bool);
-void IsBypass(bool);
-void IsrSilence(); // Function prototype for the interrupt service routine
+void IsNormal(bool); // Function prototype for the Normal state
+void IsAlarm(bool);  // Function prototype for the Alarm state
+void IsBypass(bool); // Function prototype for the Bypass state
+void IsrSilence();   // Function prototype for the interrupt service routine
+void getDebug();     // Function prototype for debugging output
 
 void PinModeSetup()
 {
     pinMode(_SilencePin, INPUT_PULLUP); // Set Silence pin as input with pull-up resistor
-    pinMode(_WaterPin, INPUT_PULLUP);   // Set Water pin as input with
+    pinMode(_WaterPin, INPUT_PULLUP);   // Set Water pin as input with pull-up resistor
     pinMode(_BuzzerPin, OUTPUT);        // Set Buzzer pin as output
     pinMode(_LedPin, OUTPUT);           // Set Led pin as output
 }
@@ -105,21 +107,22 @@ void loop()
 
         // Call the approrate function based on state
         //  int call = static_cast<int>(CurrentState);  //cast currentstate enum to int
-        States[(uint8_t)currentAlarmState](1); // Not sure if this casts the current state into an uint8_t or fetch's the equivalent
+        States[(uint8_t)currentAlarmState](1); // call the appropriate function based on the current alarm state.  The bool value is not used in this case, but it is passed to match the function signature.
+    }
+    if (Debug.Tick()){
+        getDebug();
     }
 }
 
 // Subroutine
 void IsNormal(bool i)
 {
-    Serial.println("IsNormal");
-    analogWrite(_BuzzerPin, VolumeOff);
-    analogWrite(_LedPin, LedHalf);
+    digitalWrite(_LedPin, LedFull); // Turn off the LED
+    analogWrite(_BuzzerPin, VolumeOff); // Turn off the buzzer
 }
 
 void IsAlarm(bool i)
 {
-    Serial.println("IsAlarm");
     analogWrite(_BuzzerPin, VolumeOn);
     analogWrite(_LedPin, LedFull);
     delay(AlarmF_Rate);
@@ -129,10 +132,21 @@ void IsAlarm(bool i)
 
 void IsBypass(bool i)
 {
-    Serial.println("IsBypass");
     analogWrite(_BuzzerPin, VolumeOff);
     analogWrite(_LedPin, LedFull);
     delay(BypassF_Rate);
     analogWrite(_LedPin, 0);
     delay(BypassF_Rate);
+}
+
+void getDebug()
+{
+    Serial.print("WaterFlag: ");
+    Serial.println(WaterFlag);
+    Serial.print("SilenceFlag: ");
+    Serial.println(SilenceFlag);
+    Serial.print("TimeOut: ");
+    Serial.println(TimeOut);
+    Serial.print("Current Alarm State: ");
+    Serial.println((uint8_t)currentAlarmState);
 }
